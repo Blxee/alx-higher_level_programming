d6390d77203211c7acfee49623cb41956ba907f0
Blxee <aminedev57@gmail.com>
2023-06-08 12:07:15 +0100

    Fixed: 2-args.py task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == '__main__':
     if len(argv) == 1:
         print('0 arguments.')
     else:
-        print('{} arguments:'.format(len(argv) - 1))
+        if len(argv) == 2:
+            print('{} argument:'.format(len(argv) - 1))
+        else:
+            print('{} arguments:'.format(len(argv) - 1))
         for i, arg in list(enumerate(argv))[1:]:
             print('{}: {}'.format(i, arg))
