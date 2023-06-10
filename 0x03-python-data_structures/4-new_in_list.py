920b57c4138d4bec6d781c55ea980baaba0e81f8
Blxee <aminedev57@gmail.com>
2023-06-10 10:38:12 +0100

    Fixed: 4-new_in_list.py task

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def new_in_list(my_list: list[int], idx, element):
+def new_in_list(my_list, idx, element):
     my_list = my_list.copy()
     if 0 <= idx < len(my_list):
         my_list[idx] = element
