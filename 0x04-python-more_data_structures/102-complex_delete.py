9c7ad7c8df3c4802d91c97f710b5aadfed73866b
Blxee <aminedev57@gmail.com>
2023-06-14 10:57:49 +0100

    Fixed: 102-complex_delete.py task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary: dict, value):
-    for k, v in a_dictionary.items():
+    for k, v in list(a_dictionary.items()).copy():
         if v == value:
             a_dictionary.pop(k)
     return a_dictionary
