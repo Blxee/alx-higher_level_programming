10eccdc322ef1398f26b576938e228b3a38e1718
Blxee <aminedev57@gmail.com>
2023-06-27 11:40:21 +0100

    Added: 1-square.py task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module that defines an empty Square class"""
+
+
+class Square:
+    """Class defines a square object"""
+
+    def __init__(self):
+        self.__size = None
