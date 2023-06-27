fa0d5c57814ab4440eea2f1ff93cb6563cb531a8
Blxee <aminedev57@gmail.com>
2023-06-27 11:31:37 +0100

    Fixed: 0-square.py task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A module that defines an empty Square class"""
+
+
 class Square:
     """This class litteraly serves no purpose,
     and i am writing this just becuase i have to
