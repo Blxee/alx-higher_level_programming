3a021bb022023dc1f0535514c4205e543730cacb
Blxee <aminedev57@gmail.com>
2023-06-27 15:07:13 +0100

    Fixed: 103-magic_class.py task

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,7 +2,10 @@
 """A module that defines a magic class"""
 import math
 
+
 class MagicClass:
+    """A magic class ;)"""
+
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
@@ -11,7 +14,15 @@ class MagicClass:
             self.__radius = radius
 
     def area(self):
+        """Calculates the area of the circle
+        Returns:
+            the resulted area
+        """
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """Calculates the circumference of the circle
+        Retruns:
+            the resulted circumference
+        """
         return 2 * math.pi * self.__radius
