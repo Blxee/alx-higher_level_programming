ee3e8b098e80795653a1338f5857d78967e12d68
Blxee <aminedev57@gmail.com>
2023-07-01 13:54:10 +0100

    Added: 2-matrix_divided.py task (and its tests)

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for add_integer function"""
 
+
 def add_integer(a, b=98):
     """Adds two numbers
 
@@ -12,9 +13,9 @@ def add_integer(a, b=98):
 
     Raises: TypeError if args are not if the correct type
     """
-    if not isinstance(a, int | float):
+    if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
-    if not isinstance(b, int | float):
+    if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
     a, b = int(a), int(b)
     return a + b
