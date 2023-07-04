1ac9b3ee83cd669bcd752b3e7f07b24966bbcc78
Blxee <aminedev57@gmail.com>
2023-07-04 13:55:15 +0100

    Added: 101-locked_class.py task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""Moduke for the LockedClass class"""
+
+
 class LockedClass:
+    """LockedClass preventsbthe user from creating attributes"""
+
     def __setattr__(self, name, value):
         if name == 'first_name':
-            self.__dict__[name] = value
+            super().__setattr__(name, value)
         else:
             raise AttributeError(
                 f"'LockedClass' object has no attribute'{name}'")
