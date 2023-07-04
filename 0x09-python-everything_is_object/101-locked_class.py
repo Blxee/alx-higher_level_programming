73a958f1cb3b914eeaef6d7aa6483f5ca56ace80
Blxee <aminedev57@gmail.com>
2023-07-04 15:10:40 +0100

    Fixed: 101-locked_class.py task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,10 +5,3 @@
 class LockedClass:
     """LockedClass prevents the user from creating attributes"""
     __slots__ = ['first_name']
-
-    def __setattr__(self, name, value):
-        if name != 'first_name' and not hasattr(self, name):
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no attribute '{name}'"
-            )
-        super().__setattr__(name, value)
