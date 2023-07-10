d684d9e16bb9b7d533fd3dfd4f095c5286e6bd42
Blxee <aminedev57@gmail.com>
2023-07-10 13:36:42 +0100

    Added: 10-square.py task

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,5 +8,12 @@ class Square(Rectangle):
     """class that defines a square which inhirits from Rectangle"""
 
     def __init__(self, size):
-        self.integer_validate('size', size)
-        self.__width = self.__height = self.__size = size
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the Square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        return f'[Rectangle] {self.__size}/{self.__size}'
