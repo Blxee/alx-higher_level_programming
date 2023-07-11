f935be9d0352f8cf109ae1a6df81fa10d6b1659b
Blxee <aminedev57@gmail.com>
2023-07-11 12:21:49 +0100

    Fixed: 7-add_item.py task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,13 +3,15 @@
 
 
 from sys import argv
+import os
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
 argv = argv[1:]
-filename = 'add_item.json'
+filename = './add_item.json'
 
-save([], filename)
-my_list: list = load(filename)
+if not os.path.exists(filename):
+    save([], filename)
+my_list = load(filename)
 my_list.extend(argv)
 save(my_list, filename)
