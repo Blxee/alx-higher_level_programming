a322e8cc9d12b639b068c3d2fa71108eb0f1c235
Blxee <aminedev57@gmail.com>
2023-07-15 14:33:16 +0100

    Added: Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Module for the ``Square`` class"""
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class that represents a Square
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """
+        getter for the ``size`` attribute
+        Returns:
+            the value of ``size`` attribute
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        setter for the ``size`` attribute
+        Args:
+            value (int): the new value to be assigned to size
+        Raises:
+            TypeError: if value is not int
+            ValueError: if value is <= 0
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        updates the attributes of the square
+        Args:
+            *args: the arguments tuple
+            *kwargs: the keyword arguments dict
+        """
+        attrs = ('id', 'size', 'x', 'y')
+        if len(args) > 0:
+            for attr, value in zip(attrs, args):
+                setattr(self, attr, value)
+        else:
+            for attr, value in kwargs.items():
+                if attr in attrs:
+                    setattr(self, attr, value)
+
+    def __str__(self):
+        id = self.id
+        size = self.width
+        x, y = self.x, self.y
+        return f'[Square] ({id}) {x}/{y} - {size}'
