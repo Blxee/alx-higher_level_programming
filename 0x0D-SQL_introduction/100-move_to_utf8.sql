5f8bf8799c7d7200dccf1187a69d862f3c0e7299
Blxee <aminedev57@gmail.com>
2023-08-15 14:56:42 +0100

    Added: 100-move_to_utf8.sql task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,13 +1,15 @@
 -- Go to UTF8
 ALTER DATABASE hbtn_0c_0
-CHARSET utf8mb4
+CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
+USE hbtn_0c_0
+
 ALTER TABLE first_table
-CONVERT TO CHARSET utf8mb4
+CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
-CHARSET utf8mb4
+CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
