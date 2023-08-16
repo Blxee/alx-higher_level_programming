2ae197ea9a4cb06a825892978f9a197b1f1db892
Blxee <aminedev57@gmail.com>
2023-08-16 23:28:04 +0100

    Fixed: first 2 tasks

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- My privilegges!
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- My privileges!
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
