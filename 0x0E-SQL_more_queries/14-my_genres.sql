1bbd8c2f547bb65656c7f8b95285ddf0588a964f
Blxee <aminedev57@gmail.com>
2023-08-16 15:25:22 +0100

    Added: 14-my_genres.sql task

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- My genres
+SELECT tv_genres.name
+FROM tv_show_genres
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.name = 'Dexter'
+ORDER BY tv_genres.name;
