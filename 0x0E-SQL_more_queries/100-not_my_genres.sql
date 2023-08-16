6788044a59ad27d162b2f3e50849bf81e75e0730
Blxee <aminedev57@gmail.com>
2023-08-16 23:44:15 +0100

    Added: 100-not_my_genres.sql task

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@ SELECT tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-FULL OUTER JOIN tv_shows
+INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title != 'Dexter'
 ORDER BY tv_genres.name;
