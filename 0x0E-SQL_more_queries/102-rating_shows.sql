5c15293a26b44fe893645148b268f38768afad98
Blxee <aminedev57@gmail.com>
2023-08-17 00:21:11 +0100

    Added: 102-rating_shows.sql task

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,9 @@
+-- Rotten tomatoes
+SELECT
+  tv_shows.title,
+  SUM(tv_show_rating.rate) as rating
+FROM tv_shows
+INNER JOIN tv_show_rating
+ON tv_shows.id = tv_show_rating.show_id
+GROUP BY tv_shows.title
+ORDER BY rating;
