432ab23426184f871b7a42e1b8c8d20cf7387821
Blxee <aminedev57@gmail.com>
2023-08-17 00:22:09 +0100

    Added: 102-rating_shows.sql task

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- Rotten tomatoes
 SELECT
   tv_shows.title,
-  SUM(tv_show_rating.rate) as rating
+  SUM(tv_show_rating.rate) AS rating
 FROM tv_shows
 INNER JOIN tv_show_rating
 ON tv_shows.id = tv_show_rating.show_id
