47d3cd073e4b85e17758a871dda9f35bcd3f07fa
Blxee <aminedev57@gmail.com>
2023-08-17 01:33:11 +0100

    Added: 100-not_my_genres.sql task

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,7 +4,7 @@ FROM tv_genres
 WHERE id NOT IN (
   SELECT tv_genres.id
   FROM tv_genres
-  INNER JOIN tv_show_genres
+  LEFT JOIN tv_show_genres
   ON tv_show_genres.genre_id = tv_genres.id
   INNER JOIN tv_shows
   ON tv_show_genres.show_id = tv_shows.id
