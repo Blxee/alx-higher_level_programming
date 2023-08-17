d9a15bbe0825fbf86100eddb0592f3547dc49336
Blxee <aminedev57@gmail.com>
2023-08-17 02:01:39 +0100

    Added: 100-not_my_genres.sql task

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,13 +1,13 @@
 -- Not my genre
 SELECT DISTINCT name
 FROM tv_genres
-WHERE id NOT IN (
-  SELECT DISTINCT tv_genres.id
+WHERE name NOT IN (
+  SELECT tv_genres.name
   FROM tv_genres
   LEFT JOIN tv_show_genres
   ON tv_genres.id = tv_show_genres.genre_id
   INNER JOIN tv_shows
   ON tv_show_genres.show_id = tv_shows.id
-  WHERE tv_genres.name = 'Dexter'
+  WHERE tv_shows.title = 'Dexter'
 )
 ORDER BY name;
