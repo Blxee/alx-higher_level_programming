a9ccf31376175683aabd02f94a6465e7d21aaa58
Blxee <aminedev57@gmail.com>
2023-09-11 13:47:21 +0100

    Added: 8-square.js task

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const size = parseInt(process.argv[2]);
 if (size) {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-      process.stdout.write('#');
+      process.stdout.write('X');
     }
     process.stdout.write('\n');
   }
