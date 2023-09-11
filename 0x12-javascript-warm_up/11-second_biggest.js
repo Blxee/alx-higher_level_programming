909077d9c10f5a335396d29bd17ee7668049bdec
Blxee <aminedev57@gmail.com>
2023-09-12 00:52:23 +0100

    Added: 11-second_biggest.js task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 let arr = process.argv.slice(2);
+arr = arr
+  .map(n => parseInt(n))
+  .filter(n => !isNaN(n));
 if (arr.length <= 1) {
   console.log(0);
 } else {
-  arr = arr
-    .map(n => parseInt(n))
-    .filter(n => !isNaN(n));
   arr.pop(Math.max(...arr));
   console.log(Math.max(...arr));
 }
