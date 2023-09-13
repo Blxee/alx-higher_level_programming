8a8b1aed6cd21608daae2f01ba2f4d536eeb058b
Blxee <aminedev57@gmail.com>
2023-09-13 09:59:47 +0100

    Added: 4-rectangle.js task

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let _i = 0; _i < this.height; _i++) {
+      for (let _j = 0; _j < this.width; _j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+}
+module.exports = Rectangle;
