68ca0293d63b3a25cd0e06c32df7161c14dd081e
Blxee <aminedev57@gmail.com>
2023-09-14 12:17:00 +0100

    Fixed: scripts executed when imported

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,15 +3,16 @@
 import MySQLdb
 from sys import argv
 
-with MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        charset="utf8") as conn:
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
+if __name__ == '__main__':
+    with MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8") as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+            query_rows = cur.fetchall()
+            for row in query_rows:
+                print(row)
