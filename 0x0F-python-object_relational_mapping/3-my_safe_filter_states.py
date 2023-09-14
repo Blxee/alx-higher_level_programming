336108210484d5272d552641dee74f3b9e638f15
Blxee <aminedev57@gmail.com>
2023-09-14 12:31:34 +0100

    Added: 3-my_safe_filter_states.py task

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+"""3. SQL Injection..."""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    with MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8") as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                        (argv[4],))
+            query_rows = cur.fetchall()
+            for row in query_rows:
+                print(row)
