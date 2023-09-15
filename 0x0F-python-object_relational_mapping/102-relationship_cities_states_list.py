0efd561c53d1286fb2d128edf5abf28045c81d90
Blxee <aminedev57@gmail.com>
2023-09-15 14:29:11 +0100

    Added: 102-relationship_cities_states_list.py task

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""17. From city"""
+from sys import argv
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    engine = create_engine(
+        f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+    cities = Table('cities', MetaData(), autoload_with=engine)
+    states = Table('states', MetaData(), autoload_with=engine)
+    joined = cities.join(states, cities.c.state_id == states.c.id)
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        result = session.query(joined)
+        for row in result:
+            print(f'{row[cities.c.id]}: '
+                  + f'{row[cities.c.name]} -> '
+                  + f'{row[states.c.name]}')
