f8c79607ce3150364598c07b93a37d348d22d7f7
Blxee <aminedev57@gmail.com>
2023-09-15 12:07:33 +0100

    Added: 5-filter_cities.py task

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""5. All cities by state"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    with MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8") as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT cities.id, cities.name, states.name " +
+                "FROM cities INNER JOIN states " +
+                "ON cities.state_id = states.id " +
+                "WHERE states.name LIKE BINARY %s " +
+                "ORDER BY cities.id ASC"
+                , (argv[4],))
+            query_rows = cur.fetchall()
+            for row in query_rows:
+                print(row)
