485783b33cebedd6fe6718d72ed67aa302f5cc31
Blxee <aminedev57@gmail.com>
2023-09-28 14:35:08 +0100

    Added: 5-post_params.sh task

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 5. cURL POST parameters
-curl -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" $1
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" $1
