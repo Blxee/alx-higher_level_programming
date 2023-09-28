04e3e6a54e2cf25f4777769240a67f3dd3ed9ac7
Blxee <aminedev57@gmail.com>
2023-09-28 16:01:41 +0100

    Added: 6-peak.txt task

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module for the find_peak() function """
+
+def find_peak(list_of_integers):
+    """Determines the peak integer in a list"""
+    peak = None
+    for i in list_of_integers:
+        if peak is None or i > peak:
+            peak = i
+        else:
+            break
+    return peak
