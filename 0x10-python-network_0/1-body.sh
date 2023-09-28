c5d310c72bb5f50143e5f1537c941c87e9f55bf4
Blxee <aminedev57@gmail.com>
2023-09-28 16:43:36 +0100

    Added: 1-body.sh task

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # 1. cURL to the end
-code="$(curl -sIX GET $1 | head -1 | cut -d' ' -f2)"
-if (( code == 200 ))
-then
-  curl -sX GET $1
-fi
+(( $(curl -sIX GET $1 | head -1 | cut -d' ' -f2) == 200 )) && curl -sX GET $1
