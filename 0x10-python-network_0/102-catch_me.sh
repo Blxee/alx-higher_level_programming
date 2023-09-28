d0533cdb6bb0d22bfd86e5be64383abb895561b4
Blxee <aminedev57@gmail.com>
2023-09-28 22:52:44 +0100

    Added: 102-catch_me.sh task

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 9. Catch me if you can!
-curl -sX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -LsX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
