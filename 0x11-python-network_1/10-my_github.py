5c4b0b9c2bcf2dce0ec93a51a00101d47db4eeec
Blxee <aminedev57@gmail.com>
2023-09-29 16:53:58 +0100

    Added: 10-my_github.py task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,9 +6,7 @@ from sys import argv
 if __name__ == "__main__":
     usr = argv[1]
     pwd = argv[2]
-    url = f'https://api.github.com/users/{usr}'
-    headers = {'Authorization': f'{usr} {pwd}'}
-    response = requests.get(url, headers=headers).text
-    import json
-    data = json.loads(response)
+    url = f'https://api.github.com/user'
+    response = requests.get(url, auth=(usr, pwd))
+    data = response.json()
     print(data.get('id'))
