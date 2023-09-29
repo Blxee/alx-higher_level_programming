47ab766b1d216f96b24578a92b479e90070876be
Blxee <aminedev57@gmail.com>
2023-09-29 15:47:05 +0100

    Added: 6-post_email.py task

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     url = argv[1]
     email = argv[2]
     data = {'email': email}
-    requests.options(url, data=data)
-    response = requests.get(url, data)
+    response = requests.get(url, data=data)
     text = response.text
     print(text)
