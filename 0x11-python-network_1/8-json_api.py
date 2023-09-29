cf0fa7e1af76ee22185dc2b082ba10567fbd62b4
Blxee <aminedev57@gmail.com>
2023-09-29 16:29:18 +0100

    Added: 8-json_api.py task

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,13 +2,13 @@
 """ 8. Search API """
 import requests
 from sys import argv
-import json
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     letter = argv[1] if len(argv) >= 2 else ''
     data = {'q': letter}
     response = requests.get(url, data=data)
+    import json
     try:
         dict = json.loads(response.text)
         if len(dict) == 0:
