28f544bbb52a4ee4aa95c846d0581d1f5145c1bd
Blxee <aminedev57@gmail.com>
2023-09-29 14:42:45 +0100

    Added: 100-github_commits.py task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,13 +2,13 @@
 """ 10. Time for an interview! """
 from urllib import request
 from sys import argv
-import json
 
 if __name__ == "__main__":
     repo = argv[1]
     owner = argv[2]
     url = f'https://api.github.com/repos/{owner}/{repo}/commits'
     with request.urlopen(url) as response:
+        import json
         data = json.load(response)
         for commit in data:
             print(commit.get('sha'),
