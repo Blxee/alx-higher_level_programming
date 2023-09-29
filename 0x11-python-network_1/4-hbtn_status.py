cbc12fffd9a178cb4f40dfc968ab85ba444d0eab
Blxee <aminedev57@gmail.com>
2023-09-29 15:29:11 +0100

    Added: 4-hbtn_status.py task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ 4. What's my status? #1 """
-from urllib import request
+import requests
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    with request.urlopen(url) as response:
-        text = response.read().decode('utf-8')
-        print('Body response:')
-        print('\t- type:', type(text))
-        print('\t- content:', text)
+    response = requests.get(url)
+    text = response.text
+    print('Body response:')
+    print('\t- type:', type(text))
+    print('\t- content:', text)
