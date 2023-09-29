9507cec93b7e49d7f6183f65cfaff9f98ab1de06
Blxee <aminedev57@gmail.com>
2023-09-29 15:36:01 +0100

    Added: res task

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 2. POST an email #0 """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    data = {'email': email}
+    requests.options(url, params=data)
+    response = requests.get(url, data)
+    text = response.text
+    print(text)
