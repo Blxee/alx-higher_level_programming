73b9d2386fddc64d36cbb525c533b641c5d08863
Blxee <aminedev57@gmail.com>
2023-09-29 15:26:09 +0100

    Added: 5-hbtn_header.py task

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ 5. Response header value #1 """
-from urllib import request
+import requests
 from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    response = request.urlopen(url)
+    response = requests.get(url)
     headers = response.headers
     print(headers.get('X-Request-Id'))
