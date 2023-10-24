20c3afd7d488fadb4c6bd34e3b9ce8c522a462e1
Blxee <aminedev57@gmail.com>
2023-10-24 22:40:15 +0100

    Added: task 8 (101-starwars_characters.js) of 0x14-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,18 +3,24 @@ const request = require('request');
 
 const film = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 const options = { json: true };
+let len = 0;
+let characters = [];
 
-request(film, (err, response, body) => {
+request(film, options, (err, response, body) => {
+  len = body.characters.length;
+  characters = body.characters;
   if (!err) {
-    body = JSON.parse(body);
-    body.characters.forEach(character => {
-      request(character, options, (err, response, body) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log(body.name);
-      });
-    });
+    recurse(0);
   }
 });
+
+function recurse (index) {
+  request(characters[index], options, (err, response, body) => {
+    if (!err) {
+      console.log(body.name);
+      if (index < len - 1) {
+        recurse(index + 1);
+      }
+    }
+  });
+}
